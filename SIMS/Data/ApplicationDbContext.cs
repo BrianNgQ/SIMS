ac70f5e66b4a7c44aead3709a08a486808a6f0f5
BrianNgQ <146939700+BrianNgQ@users.noreply.github.com>
2024-07-30 00:08:04 +0700

    Add project files.

---
+++ SIMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SIMS.Models;
+
+namespace SIMS.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<SIMS.Models.Semesters> Semesters { get; set; } = default!;
+        public DbSet<SIMS.Models.Departments> Departments { get; set; } = default!;
+        public DbSet<SIMS.Models.Majors> Majors { get; set; } = default!;
+        public DbSet<SIMS.Models.Subjects> Subjects { get; set; } = default!;
+        public DbSet<SIMS.Models.Courses> Courses { get; set; } = default!;
+        public DbSet<ApplicationUser> ApplicationUser { get; set; } = default!;    
+      
+    }
+}
